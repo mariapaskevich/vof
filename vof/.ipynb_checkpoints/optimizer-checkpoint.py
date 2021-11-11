8e38ecffa478d4ecd7baf206cc71dd01edafb891
mariapaskevich <jupyter@packer-5feabc72-7e50-797b-8549-cf82f531e6d0.c.deeplearning-platform.internal>
2021-11-11 22:03:15 +0000

    start of the package + child class for Electricity case with cost_function

---
+++ vof/.ipynb_checkpoints/optimizer-checkpoint.py
@@ -0,0 +1,61 @@
+import random
+import pandas as pd 
+import numpy as np
+import nevergrad as ng
+
+
+
+class OptimalDecisionMakers(): 
+    
+    def __init__(self, data, target):
+        self.x = data#.drop([target], axis=1)
+        self.y = data[target]
+    
+    def select_value_optimal_model(self,
+                                   optimizer,
+                                   cost_function,
+                                   decision_time_step,
+                                   optimization_horizon,
+                                   time_unit = 'H',
+                                   random_seed=42,
+                                   return_predictions=False):
+        
+        import random
+        
+        #random.seed(random_seed)
+        self.cost_function = cost_function
+        self.optimal_decisions_df = pd.DataFrame(index=(self.x.index),columns=self.x.columns).iloc[:-optimization_horizon]
+
+        for col in range(len(self.optimal_decisions_df.columns)):
+
+            total_steps = range(0,len(self.x)-optimization_horizon,decision_time_step)
+            recommendation = np.array([])
+
+            for step in total_steps:
+                
+                par = ng.p.Array(shape=(optimization_horizon,)).set_bounds(lower=-80, upper=80)
+                optimizer = ng.optimizers.CMA(parametrization=par, budget=5000)
+                
+                try:
+                    self.candidate = self.x.iloc[step:step+optimization_horizon,col]
+                except:
+                    print(self.optimal_decisions_df.shape)
+                    print(self.x.iloc[step:step+optimization_horizon,col])
+                    
+                step_recommendation = optimizer.minimize(cost_function)
+                recommendation = np.append(recommendation,step_recommendation.value[0:decision_time_step])
+                
+
+            self.optimal_decisions_df.iloc[:,col] = recommendation
+            
+
+        return self.optimal_decisions_df
+    
+    def evaluate(self):
+        
+        for col in range(len(self.optimal_decisions_df.columns)):
+            
+            print(self.cost_function(self.optimal_decisions_df.iloc[:,col]))
+        
+    def select_value_optimal_hyperparameters():
+        return
---
+++ vof/forecaster.py
@@ -0,0 +1,58 @@
+import pandas as pd 
+
+class RecursiveForecaster():
+    
+    def __init__(self, data, target):
+        self.x = data.drop(target, axis=1)
+        self.y = data[target]
+
+    def add_lag_features(self,lags):
+        self.lags=lags
+        for i in lags:
+            self.x['lag_'+str(i)] = self.y.shift(i)    
+            
+    
+    #def optimize(self, pred):
+    #    self.cost_function
+    
+    #def fit(self):
+        #we run our daily loops for valdation and call the optimizer
+    
+    def create_prediction(self, model_name,
+                          start_day='2019-01-02', 
+                          last_known_day='2019-12-01', 
+                          prediction_day='2019-12-02',
+                          frequency='D',
+                          horizon=10):     
+        
+        print(model_name)
+        
+        X_train = self.x.loc[start_day:last_known_day]
+        y_train = self.y.loc[start_day:last_known_day]
+        
+        
+        model_name.fit(X_train,y_train)
+
+        pred_index = pd.date_range(start=prediction_day, 
+                                   periods=horizon, 
+                                   freq=frequency)
+        
+        #print(pred_index)
+        pred_df = pd.DataFrame(index=pred_index, 
+                               columns=[self.y.name])
+        
+        #print(pred_df)
+        #Create prediction for each time step in pred_index
+        for step in pred_index:
+            indx = str(step)#[:-9]
+            #print(indx)
+            X_pred = pd.DataFrame(self.x.loc[indx]).T
+            #print(indx)
+            x_index = self.x.loc[indx].name
+            pred_df.loc[x_index] = model_name.predict(X_pred)
+            
+            #update features with lags containing lags of predicted value 
+            for i in self.lags:
+                self.x.loc[indx,'lag_'+str(i)] = pred_df.shift(i)
+
+        return #pred_df
