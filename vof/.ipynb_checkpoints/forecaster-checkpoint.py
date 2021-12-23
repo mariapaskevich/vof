2730f0a6317d026cbe42da54b722d382d5409d15
mariapaskevich <jupyter@packer-5feabc72-7e50-797b-8549-cf82f531e6d0.c.deeplearning-platform.internal>
2021-12-23 19:34:13 +0000

    Fixes for data leakage in single prediction, added recursive predictor to class RecursiveForecaster

--- vof/.ipynb_checkpoints/forecaster-checkpoint.py
+++ vof/.ipynb_checkpoints/forecaster-checkpoint.py
@@ -19,7 +19,7 @@ class RecursiveForecaster():
     #def fit(self):
         #we run our daily loops for valdation and call the optimizer
     
-    def create_prediction(self, model_name,
+    def create_single_prediction(self, model_name,
                           start_day='2019-01-02', 
                           last_known_day='2019-12-01', 
                           prediction_day='2019-12-02',
@@ -28,13 +28,20 @@ class RecursiveForecaster():
         
         print(model_name)
         
-        self.x_modified = self.x.copy()
+        x_modified = self.x.copy()
+        x_modified.loc[prediction_day:] = x_modified.loc[prediction_day:] # check to avoid leakage
         
-        X_train = self.x_modified.loc[start_day:last_known_day]
+        X_train = x_modified.loc[start_day:last_known_day]
         y_train = self.y.loc[start_day:last_known_day]
         
         
         model_name.fit(X_train,y_train)
+        
+        try:
+            self.model_coefs = model_name.coef_
+        except:
+            pass
+        
 
         pred_index = pd.date_range(start=prediction_day, 
                                    periods=horizon, 
@@ -48,15 +55,59 @@ class RecursiveForecaster():
         #Create prediction for each time step in pred_index
         for step in pred_index:
             indx = str(step)#[:-9]
-            #print(indx)
-            X_pred = pd.DataFrame(self.x_modified.loc[indx]).T
-            #print(X_pred)
-            x_index = self.x_modified.loc[indx].name
-            #print(pred_df.loc[x_index])
-            pred_df.loc[x_index] = model_name.predict(X_pred)
             
-            #update features with lags containing lags of predicted value 
-            for i in self.lags:
-                self.x_modified.loc[indx,'lag_'+str(i)] = pred_df.shift(i)
+            #print('x_modified before the loop:')
+            #print(x_modified.loc[str(pred_index[0]):])
+            
+            if step != pred_index[0]: #starting from the second step, update x_modified with lags containing lags of predicted value 
+                #print('replacing values!')
+                for i in self.lags:
+                    inx_to_replace = pred_df.shift(i).dropna().index
+                    x_modified.loc[inx_to_replace,'lag_'+str(i)] = pred_df.shift(i).loc[inx_to_replace].values
+                #print('done!')
+            
+            #print(pred_df.shift(1))
+            
+            #print('pred_df')
+            #print(pred_df)
+            #print('x_modified after the loop:')
+            #print(x_modified.loc[str(pred_index[0]):])
 
+
+            X_pred = pd.DataFrame(x_modified.loc[indx]).T
+            x_index = x_modified.loc[indx].name
+            pred_df.loc[x_index] = model_name.predict(X_pred)            
+            
+            
         return pred_df
+    
+    def create_recursive_prediction(self, model_name,
+                                      start_day='2019-01-02', 
+                                      last_known_day='2019-12-01', 
+                                      prediction_day='2019-12-02',
+                                      terminal_day='2019-12-17',
+                                      freq='D',
+                                      h=10):
+        
+        pred_days = pd.date_range(start=prediction_day, end=terminal_day, freq='D')
+
+        start_days = pd.date_range(start=start_day, periods=len(pred_days), freq='D')
+
+        last_known_days = pd.date_range(start=last_known_day, periods=len(pred_days), freq='D')
+
+        forecast_for_days = pd.DataFrame(index=self.x.loc[prediction_day:].index)
+
+        
+        for (pd_day,st_day,lk_day) in zip(pred_days,start_days,last_known_days):
+            
+            #print(pd_day,st_day,lk_day)
+            res = self.create_single_prediction(model_name,
+                              start_day=st_day,
+                              last_known_day=lk_day,
+                              prediction_day=pd_day,
+                              frequency=freq,
+                              horizon=h)
+
+            forecast_for_days[pd_day] = res
+
+        return forecast_for_days
